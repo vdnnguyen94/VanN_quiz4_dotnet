7629fde54ef9a07db259a5e79518a3fe7a741562
Van Nguyen <vdnnguyen94@gmail.com>
2025-04-05 13:34:13 -0400

    Simple ASP.NET set up

---
+++ VanNguyen-SimpleASP.NET/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using VanNguyen_SimpleASP.NET.Models;
+
+namespace VanNguyen_SimpleASP.NET.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Van Nguyen";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
